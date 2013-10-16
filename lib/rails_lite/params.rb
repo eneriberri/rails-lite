144781074dc07b8e069c099db60a8918661846fc
App Academy Student <student@appacademy.io>
2013-10-16 16:38:42 -0700

    Up until routing

--- lib/rails_lite/params.rb
+++ lib/rails_lite/params.rb
@@ -22,19 +22,83 @@ class Params
 
   private
   def parse_www_encoded_form(www_encoded_form)
+		params = {}
+
 		key_val_arr = URI.decode_www_form(www_encoded_form)
-		key_val_arr.each do |key, val|
-			@params[key] = val
+		key_val_arr.each do |nested_key, val|
+			nested = params
+
+			keys = parse_key(nested_key)
+			keys.each_with_index do |key, i|
+				if i == keys.length - 1
+					nested[key] = val
+				else
+					nested[key] ||= {}
+					nested = nested[key]
+				end
+			end
 		end
-		@params
+
+		params
   end
 
+
   def parse_key(key)
+		nested = key.split(/\]\[|\[|\]/)
+		return [nested.last] if nested.first == ""
+		return nested
   end
 end
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+#   private
+#   def parse_www_encoded_form(www_encoded_form)
+#     params = {}
+#
+#     key_values = URI.decode_www_form(www_encoded_form)
+#     key_values.each do |full_key, value|
+#       scope = params
+#
+#       key_seq = parse_key(full_key)
+#       key_seq.each_with_index do |key, idx|
+#         if (idx + 1) == key_seq.count
+#           scope[key] = value
+#         else
+#           scope[key] ||= {}
+#           scope = scope[key]
+#         end
+#       end
+#     end
+#
+#     params
+# #   end
+
+
+
+
+
+#  def parse_key(key)
+#     match_data = /(?<head>.*)\[(?<rest>.*)\]/.match(key)
+#     if match_data
+#       parse_key(match_data["rest"]).unshift(match_data["head"])
+#     else
+#       [key]
+#     end
+#   end
 
 
 
